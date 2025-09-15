9ad920ccd7311d71846b6aaf9d5d6c27d57f1fb4
natividadlolhaha <endlessparadox2010@gmail.com>
2025-09-15 17:54:26 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from pyscript import document
+
+# Show store info automatically when page loads
+document.getElementById("storeName").innerText = "Gab's Ramen House"
+document.getElementById("ownerName").innerText = "Owner: Gabriel Natividad"
+document.getElementById("yearFounded").innerText = "Founded: 2025"
